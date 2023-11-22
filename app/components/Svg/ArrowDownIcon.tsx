79cd8ebf77f2a09d44de4a04daf82c25355d69a3
Devesh Mhatre <deveshmhtr.mhatre92@gmail.com>
2023-11-22 17:03:25 +0530

    Implement ratings section of ProductScreen
    - Scale up go back image

--- app/components/Svg/ArrowDownIcon.tsx
+++ app/components/Svg/ArrowDownIcon.tsx
@@ -1,16 +1,19 @@
 import { Path, Svg } from 'react-native-svg';
 
-import Colors from '../../helpers/Colors';
-
 export default function ArrowDownIcon({
   goBack = false,
 }: {
   goBack?: boolean;
 }) {
   return (
-    <Svg width={9} height={5} fill="none">
+    <Svg
+      width={9}
+      height={5}
+      fill="none"
+      style={{ transform: goBack ? [{ scale: 1.5 }] : [{ scale: 1 }] }}
+    >
       <Path
-        stroke={goBack ? Colors.black.default : '#B2BBCE'}
+        stroke={goBack ? '#000' : '#B2BBCE'}
         d="m1 .757 3.471 3.486L7.942.757"
       />
     </Svg>
--- app/screens/home/ProductScreen.tsx
+++ app/screens/home/ProductScreen.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { StarRatingDisplay } from 'react-native-star-rating-widget';
+
+import Colors from '../../helpers/Colors';
+import Fonts from '../../helpers/Fonts';
 
 import { HomeStackParamList } from './HomeScreen';
 import CartButton from '../../components/Shared/CartButton';
 import GoBackButton from '../../components/Shared/GoBackButton';
+import StarIcon from '../../components/Svg/StarIcon';
 
 type ProductScreenProps = NativeStackScreenProps<HomeStackParamList, 'Product'>;
 
@@ -23,6 +28,40 @@ export default function ProductScreen({
           inverse={true}
         />
       </View>
+
+      <ScrollView
+        style={{
+          marginHorizontal: 20,
+          marginTop: 20,
+          marginBottom: 100,
+        }}
+      >
+        <Text style={styles.productTitle}>{product.title}</Text>
+        <View
+          style={{
+            marginTop: 8,
+            flexDirection: 'row',
+            alignItems: 'flex-start',
+          }}
+        >
+          <StarRatingDisplay
+            rating={product.rating}
+            color={Colors.yellow.darker}
+            starSize={30}
+            StarIconComponent={(props) => <StarIcon {...props} />}
+          />
+          <Text
+            style={{
+              fontFamily: Fonts.ManropeRegular,
+              fontSize: 14,
+              includeFontPadding: false,
+              color: '#A1A1AB',
+            }}
+          >
+            110 Reviews
+          </Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -39,5 +78,11 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+  },
+  productTitle: {
+    fontFamily: Fonts.ManropeBold,
+    fontSize: 50,
+    includeFontPadding: false,
+    color: Colors.black.default,
   },
 });
